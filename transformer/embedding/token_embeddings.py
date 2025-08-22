e778c80953f6ff7a4627fb5729fb1df3648b9191
hkwuks <25031515+hkwuks@users.noreply.github.com>
2025-08-22 16:09:45 +0800

    update

--- transformer/embedding/token_embeddings.py
+++ transformer/embedding/token_embeddings.py
@@ -8,3 +8,7 @@ class TokenEmbedding(nn.Embedding):
         :param d_model: dimensions of model
         '''
         super(TokenEmbedding, self).__init__(vocab_size, d_model, padding_idx=1)
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding(x)
